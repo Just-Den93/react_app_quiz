8f6b5b2ca53ec79c0c25e1a56ca31c0318605e16
Denis <55592373+Just-Den93@users.noreply.github.com>
2024-11-21 17:38:51 +0200

    21/11/24/18:00

--- src/components/common/ModalManager/ModalManager.tsx
+++ src/components/common/ModalManager/ModalManager.tsx
@@ -1,79 +1,79 @@
-import React from 'react';
-import { useModal } from '../../../context/ModalContext';
-import { useQuizContext } from '../../../context/QuizContext';
-import MenuModal from '../MenuModal/MenuModal';
-import Modal from '../Modal/Modal';
-import Settings from '../../features/Game/Settings/Settings';
-import EndMessage from '../../features/Game/Messages/EndMessage/EndMessage';
-import ConfettiAnimation from '../../features/Game/Animation/ConfettiAnimation';
-import { Category, QuizBlock } from '../../../types/quiz.types';
+	import React from 'react';
+	import { useModal } from './useModal';
+	import { useQuizContext } from '../../../context/QuizContext';
+	import MenuModal from '../MenuModal/MenuModal';
+	import Modal from '../Modal/Modal';
+	import Settings from '../../features/Game/Settings/Settings';
+	import EndMessage from '../../features/Game/Messages/EndMessage/EndMessage';
+	import ConfettiAnimation from '../../features/Game/Animation/ConfettiAnimation';
+	import { Category, QuizBlock } from '../../../types/quiz.types';
 
-interface ModalManagerProps {
-  selectedBlock: QuizBlock | null;
-  selectedCategory: Category | null;
-  isBlockUsed: boolean;
-  onModalClose: () => void;
-  onBlockRetry: () => void;
-  onSelectCategory: (categoryId: string, blockId: number) => void;
-  onNewGame: () => void;
-  onMainMenu: () => void;
-}
+	interface ModalManagerProps {
+	selectedBlock: QuizBlock | null;
+	selectedCategory: Category | null;
+	isBlockUsed: boolean;
+	onModalClose: () => void;
+	onBlockRetry: () => void;
+	onSelectCategory: (categoryId: string, blockId: number) => void;
+	onNewGame: () => void;
+	onMainMenu: () => void;
+	}
 
-export const ModalManager: React.FC<ModalManagerProps> = ({
-  selectedBlock,
-  selectedCategory,
-  isBlockUsed,
-  onModalClose,
-  onBlockRetry,
-  onSelectCategory,
-  onNewGame,
-  onMainMenu
-}) => {
-  const { state, closeSettings, closeMenu } = useModal();
-  const { currentQuizId, selectedMode, setQuizStates } = useQuizContext();
+	export const ModalManager: React.FC<ModalManagerProps> = ({
+	selectedBlock,
+	selectedCategory,
+	isBlockUsed,
+	onModalClose,
+	onBlockRetry,
+	onSelectCategory,
+	onNewGame,
+	onMainMenu
+	}) => {
+	const { state, closeSettings, closeMenu } = useModal();
+	const { currentQuizId, selectedMode, setQuizStates } = useQuizContext();
 
-  if (!currentQuizId && state.endMessage) {
-    console.error('Quiz ID missing for end message');
-    return null;
-  }
+	if (!currentQuizId && state.endMessage) {
+		console.error('Quiz ID missing for end message');
+		return null;
+	}
 
-  return (
-    <>
-      <ConfettiAnimation isRunning={state.confetti} />
+	return (
+		<>
+			<ConfettiAnimation isRunning={state.confetti} />
 
-      {selectedBlock && (
-        <Modal
-          block={selectedBlock}
-          categoryName={selectedCategory?.name ?? 'Без категории'}
-          onClose={onModalClose}
-          selectedMode={selectedMode ?? 1}
-          onSelectCategory={onSelectCategory}
-          isBlockUsed={isBlockUsed}
-          onTryAgain={onBlockRetry}
-          onContinue={onModalClose}
-        />
-      )}
+			{selectedBlock && (
+			<Modal
+				block={selectedBlock}
+				categoryName={selectedCategory?.name ?? 'Без категории'}
+				onClose={onModalClose}
+				selectedMode={selectedMode ?? 1}
+				onSelectCategory={onSelectCategory}
+				isBlockUsed={isBlockUsed}
+				onTryAgain={onBlockRetry}
+				onContinue={onModalClose}
+			/>
+			)}
 
-      {state.menu && (
-        <MenuModal
-          showSettings={closeMenu}
-          showMainMenu={onMainMenu}
-          onNewGame={onNewGame}
-          isVisible={state.menu}
-          closeMenuModal={closeMenu}
-        />
-      )}
+			{state.menu && (
+			<MenuModal
+				showSettings={closeMenu}
+				showMainMenu={onMainMenu}
+				onNewGame={onNewGame}
+				isVisible={state.menu}
+				closeMenuModal={closeMenu}
+			/>
+			)}
 
-      {state.settings && <Settings onClose={closeSettings} />}
+			{state.settings && <Settings onClose={closeSettings} />}
 
-      {state.endMessage && currentQuizId && (
-        <EndMessage
-          currentQuizId={currentQuizId}
-          setQuizStates={setQuizStates}
-          onNewGame={onNewGame}
-          onMainMenu={onMainMenu}
-        />
-      )}
-    </>
-  );
-};
+			{state.endMessage && currentQuizId && (
+			<EndMessage
+				currentQuizId={currentQuizId}
+				setQuizStates={setQuizStates}
+				onNewGame={onNewGame}
+				onMainMenu={onMainMenu}
+			/>
+			)}
+		</>
+	);
+	};
